3a8660812650e5cc58014b837b168b5d85a9724c
lining <lo@qq.com>
2025-04-16 17:03:04 +0800

    feat: Main 组件 使用 layout 布局

--- pzadmin/src/views/Main.vue
+++ pzadmin/src/views/Main.vue
@@ -1,14 +1,18 @@
 <template>
-    <div>
-      main
+    <div class="common-layout">
+        <el-container>
+            <el-aside width="200px">Aside</el-aside>
+            <el-container>
+                <el-header>Header</el-header>
+                <el-main>Main</el-main>
+            </el-container>
+        </el-container>
     </div>
-  </template>
-  
-  <script setup>
-  import { ref, reactive} from 'vue'
-  
-  </script>
-  
-  <style lang='scss' scoped>
-  
-  </style>
+</template>
+
+<script setup>
+import { ref, reactive } from 'vue'
+
+</script>
+
+<style lang='scss' scoped></style>
