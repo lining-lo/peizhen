1d2852a9bf6582ec92ba70305eb8a239382b6e57
lining <lo@qq.com>
2025-04-16 11:21:20 +0800

    配置 Element Plus 按需引入

--- pzadmin/src/views/Login/index.vue
+++ pzadmin/src/views/Login/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     登录
+    <el-button disabled>Default</el-button>
+    <el-button type="primary" disabled>Primary</el-button>
+    <el-button type="success" disabled>Success</el-button>
+    <el-button type="info" disabled>Info</el-button>
+    <el-button type="warning" disabled>Warning</el-button>
+    <el-button type="danger" disabled>Danger</el-button>
   </div>
 </template>
 
