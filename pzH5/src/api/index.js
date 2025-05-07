44d68df712810dc49edee58b362d0a521ff17e32
lining <lo@qq.com>
2025-05-07 20:46:35 +0800

    feat: 首页组件的搭建

--- pzH5/src/api/index.js
+++ pzH5/src/api/index.js
@@ -1,7 +1,13 @@
 import http from '../utils/request'
 
 export default {
+    //用户登录
     login(data) {
         return http.post('/login', data)
     },
+    
+    //首页数据
+    index(){
+        return http.get('/Index/index')
+    }
 }
